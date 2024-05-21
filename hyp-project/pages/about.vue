f69fd8c3d82c06d9374ae063deb75ed678e8bee2
luciacameno <chancameno@gmail.com>
2024-05-21 14:25:28 +0200

    About_us update

--- hyp-project/pages/about.vue
+++ hyp-project/pages/about.vue
@@ -2,6 +2,7 @@
   <div class="wrapper">
     <div class="container">
 
+
       <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
