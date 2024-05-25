1cb03d09fbc7b1114a655e33a114818fac25d186
QuiqueRobles <e.robles3@usp.ceu.es>
2024-05-25 09:28:09 +0200

    Chatbot added

--- hyp-project/layouts/default.vue
+++ hyp-project/layouts/default.vue
@@ -3,6 +3,9 @@
     <!-- Navbar -->
     <NavBar />
 
+    <!-- Chatbot -->
+    <Chatbot />
+
     <!-- Contenido de la página -->
     <div class="content">
       <!-- Aquí incluirás el contenido principal de tu aplicación, como las páginas generadas por Nuxt.js -->
@@ -17,12 +20,13 @@
 <script>
 import NavBar from '@/components/NavBar.vue'
 import Footer from '@/components/Footer.vue'
-
+import Chatbot from '@/components/Chatbot.vue'
 
 export default {
   components: {
     NavBar,
-    Footer
+    Footer,
+    Chatbot
   }
 }
 </script>
