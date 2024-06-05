62e0a1de682f9ca218eaebfa033de324fea7c851
QuiqueRobles <e.robles3@usp.ceu.es>
2024-06-05 20:28:21 +0200

    Another try, hope this is the good one

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  target: 'static',
   app: {
     head: {
       title: 'Women Support Center',
