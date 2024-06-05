575725d4ce0abe1b34b1f7d38d8a17a4f9a6f076
QuiqueRobles <e.robles3@usp.ceu.es>
2024-06-05 20:23:19 +0200

    Trying to add to github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  target: 'static',
   app: {
     head: {
       title: 'Women Support Center',
